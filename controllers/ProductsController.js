557859b234598abd430388eca8f5c73631fc447e
samankey <71953671+samankey@users.noreply.github.com>
2021-07-25 21:49:11 +0900

    Remove : 태그 버튼 API 삭제

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -4,15 +4,6 @@ const showProducts = async (req, res) => {
   try {
     const subCategoryId = req.query.subCategoryId;
     let products = await ProductsService.getProducts(subCategoryId);
-    
-    products = products.map(product=>{
-      const {is_new, is_sold_out, is_vegan} = product
-      product.tags=[]
-      if(is_new) product.tags.push("NEW")
-      if(is_sold_out) product.tags.push("SOLD OUT")
-      if(is_vegan) product.tags.push("VEGAN")
-      return product
-    })
 
     res.status(200).json({ products });
   } catch (err) {
